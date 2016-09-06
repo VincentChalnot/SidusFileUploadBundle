ada93081f0ef2dd8094e2ef3a5a28c0e11dbe8db
Vincent Chalnot <vchalnot@clever-age.com>
2016-09-06 15:33:54 +0200

    Fixed behavior of file upload field, dropped "entity" type as parent because it was loading all the possible choices in memory.

--- Form/Type/ResourceType.php
+++ Form/Type/ResourceType.php
@@ -2,10 +2,14 @@
 
 namespace Sidus\FileUploadBundle\Form\Type;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Common\Persistence\ObjectRepository;
 use Sidus\FileUploadBundle\Configuration\ResourceTypeConfiguration;
 use Sidus\FileUploadBundle\Manager\ResourceManager;
+use Sidus\FileUploadBundle\Model\ResourceInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\Options;
@@ -21,12 +25,17 @@ class ResourceType extends AbstractType
     /** @var ResourceManager */
     protected $resourceManager;
 
+    /** @var Registry */
+    protected $doctrine;
+
     /**
      * @param ResourceManager $resourceManager
+     * @param Registry        $doctrine
      */
-    public function __construct(ResourceManager $resourceManager)
+    public function __construct(ResourceManager $resourceManager, Registry $doctrine)
     {
         $this->resourceManager = $resourceManager;
+        $this->doctrine = $doctrine;
     }
 
     /**
@@ -39,14 +48,46 @@ class ResourceType extends AbstractType
         /** @var ResourceTypeConfiguration $resourceType */
         $resourceType = $options['resource_type'];
         $view->vars['endpoint'] = $resourceType->getEndpoint();
+        $view->vars['data'] = $form->getData(); // "data" is the entity and "value" the ID
     }
 
     /**
-     * @return string
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     *
+     * @throws \Exception
      */
-    public function getParent()
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        return 'entity';
+        // The view must always be an ID
+        $builder->addViewTransformer(new CallbackTransformer(
+            function ($originalData) {
+                if (!$originalData instanceof ResourceInterface) {
+                    return $originalData; // Why is this condition necessary when submitting ? Makes no sense...
+                }
+
+                return $this->getIdentifierValue($originalData);
+            },
+            function ($submittedData) {
+                return $submittedData;
+            }
+        ));
+
+        // Model data must always be an entity
+        $builder->addModelTransformer(new CallbackTransformer(
+            function ($originalData) {
+                return $originalData;
+            },
+            function ($submittedData) use ($options) {
+                /** @var ObjectRepository $repository */
+                $repository = $options['repository'];
+                if (null === $submittedData || '' === $submittedData) {
+                    return null;
+                }
+
+                return $repository->find($submittedData);
+            }
+        ));
     }
 
     /**
@@ -69,8 +110,10 @@ class ResourceType extends AbstractType
             'resource_type',
         ]);
         $resolver->setDefaults([
-            'query_builder' => $this->getQueryBuilder(),
             'class' => null,
+            'repository' => null,
+            'compound' => false,
+            'error_bubbling' => false,
         ]);
         $resolver->setNormalizer('resource_type', function (Options $options, $value) {
             return $this->resourceManager->getResourceTypeConfiguration($value);
@@ -83,15 +126,38 @@ class ResourceType extends AbstractType
 
             return $value;
         });
+        $resolver->setNormalizer('repository', function (Options $options, $value) {
+            if ($value) {
+                if ($value instanceof ObjectRepository) {
+                    return $value;
+                }
+                throw new \UnexpectedValueException("The 'repository' option must be an EntityRepository");
+            }
+            $class = $options['class'];
+            if (!$class) {
+                throw new \UnexpectedValueException("Missing option 'class' or 'repository'");
+            }
+
+            return $this->doctrine->getRepository($class);
+        });
     }
 
     /**
-     * @return \Closure
+     * @param ResourceInterface $originalData
+     *
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     *
+     * @return array
      */
-    protected function getQueryBuilder()
+    protected function getIdentifierValue(ResourceInterface $originalData)
     {
-        return function (EntityRepository $repo) {
-            return $repo->createQueryBuilder('e');
-        };
+        $metadata = $this->doctrine->getManager()->getClassMetadata(get_class($originalData));
+        $identifier = $metadata->getIdentifierValues($originalData);
+        if (count($identifier) !== 1) {
+            throw new \LogicException('ResourceInterface must have a single identifier (primary key)');
+        }
+
+        return array_pop($identifier);
     }
 }
